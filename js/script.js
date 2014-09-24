4de6d7275ddfe1dab498c53e8531e3a6f2bc44a1
Michael Toback <michael.toback@gmail.com>
2014-09-23 22:42:37 -0700

    Added link for biographical data for congressman/senator

--- js/script.js
+++ js/script.js
@@ -42,6 +42,7 @@ $(function()
 						innerHTML +="<tr>";
 						innerHTML += "<td>"+ congressman['first_name']+ " " + congressman['last_name'] + "</td>";
 						innerHTML += "<td style='text-align:center;''>" + congressman['party'] + "</td>";
+						innerHTML += "<td><a href='http://bioguide.congress.gov/scripts/biodisplay.pl?index=" + congressman['bioguide_id']+ "' target=_blank>biography</a></td>";
 						innerHTML += "<td><a href='mailto:" + congressman['oc_email']+ "' target=_blank>email</a></td>";
 						innerHTML += "<td><a href='" + congressman['contact_form']+ "' target=_blank>contact</a></td>";
 						innerHTML += "<td><a href='" + congressman['website'] + "' target=_blank'>website</a></td>";
@@ -73,6 +74,7 @@ $(function()
 						innerHTML +="<tr>";
 						innerHTML += "<td>"+ representative['first_name']+ " " + representative['last_name'] + "</td>";
 						innerHTML += "<td style='text-align:center;''>" + representative['party'] + "</td>";
+						innerHTML += "<td><a href='http://bioguide.congress.gov/scripts/biodisplay.pl?index=" + representative['bioguide_id']+ "' target=_blank>biography</a></td>";
 						innerHTML += "<td><a href='mailto:" + representative['oc_email']+ "' target=_blank>email</a></td>";
 						innerHTML += "<td><a href='" + representative['contact_form']+ "' target=_blank>contact</a></td>";
 						innerHTML += "<td><a href='" + representative['website'] + "' target=_blank'>website</a></td>";
@@ -112,6 +114,7 @@ $(function()
 						innerHTML +="<tr>";
 						innerHTML += "<td>"+ senator['first_name']+ " " + senator['last_name'] + "</td>";
 						innerHTML += "<td style='text-align:center;''>" + senator['party'] + "</td>";
+						innerHTML += "<td><a href='http://bioguide.congress.gov/scripts/biodisplay.pl?index=" + senator['bioguide_id']+ "' target=_blank>biography</a></td>";
 						innerHTML += "<td><a href='mailto:" + senator['oc_email']+ "' target=_blank>email</a></td>";
 						innerHTML += "<td><a href='" + senator['contact_form']+ "' target=_blank>contact</a></td>";
 						innerHTML += "<td><a href='" + senator['website'] + "' target=_blank'>website</a></td>";
